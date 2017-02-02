59b9a2fcf7b86401fea755f53d29eee0cd58ce5d
RiFFoo <farukhshinrifat@hotmail.com>
2017-02-02 14:24:02 +0500

    PropertyFile
    Create Property File

--- src/ru/skb_lab/Main.java
+++ src/ru/skb_lab/Main.java
@@ -1,10 +1,38 @@
 package ru.skb_lab;
-
+ 
+import java.io.*;
+import java.util.Properties;
+ 
 public class Main {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hi Jack!!");
-	}
-
+ 
+    public static void main(String[] args) {
+ 
+        FileInputStream fis;
+        Properties property = new Properties();
+ 
+        try {
+            
+        	
+        	fis = new FileInputStream("src/ru/skb_lab/Config/config.properties");
+            property.load(fis);
+ 
+            String host = property.getProperty("db.host");
+            String login = property.getProperty("db.login");
+            String password = property.getProperty("db.password");
+            String separator = property.getProperty("file.separator");
+            String in = property.getProperty("db.in");
+            String out = property.getProperty("db.out");
+            System.out.println("HOST: " + host
+                            + ", LOGIN: " + login
+                            + ", PASSWORD: " + password
+                            +", SEPARATOR:" + separator
+                            +",IN:" + in
+                            +",OUT:" + out);
+ 
+        } catch (IOException e) {
+            System.err.println("ОШИБКА: Файл свойств отсуствует!"+e.getMessage());
+        }
+ 
+    }
+ 
 }
