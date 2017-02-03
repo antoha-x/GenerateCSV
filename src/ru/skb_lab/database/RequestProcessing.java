ea6c55b75bfa9365ab0d6a622f4bc5dc03283a4c
RiFFoo <farukhshinrifat@hotmail.com>
2017-02-03 13:04:06 +0500

    DbConnect
    Data Base connection

--- src/ru/skb_lab/database/RequestProcessing.java
+++ src/ru/skb_lab/database/RequestProcessing.java
@@ -1,5 +1,24 @@
 package ru.skb_lab.database;
 
-public class RequestProcessing {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
+public class RequestProcessing {
+	
+	Connection connection = null;
+	
+	public RequestProcessing(Properties prop) {
+		try {
+		    Class.forName("oracle.jdbc.driver.OracleDriver");
+		    connection = DriverManager.getConnection(prop.getProperty("db.host"),prop.getProperty("db.login"),
+		    		prop.getProperty("db.password"));
+		} catch (ClassNotFoundException cnfe) {
+		    System.out.println("Where is your MySQL JDBC Driver?");
+		    cnfe.printStackTrace();
+		} catch (SQLException e) {
+	        e.printStackTrace();
+		}
+	}
 }
