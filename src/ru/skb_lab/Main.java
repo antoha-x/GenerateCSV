a69a2b91a82fa348c15126ef72cbf838c71525d4
antoha-x <272acy@gmail.com>
2017-02-07 17:22:14 +0500

    refactor
    1. add default Constructor in ReadFiles
    2. remove String in HashMap
    3. add constant Strings with directory (default, and user).

--- src/ru/skb_lab/Main.java
+++ src/ru/skb_lab/Main.java
@@ -7,10 +7,12 @@ import ru.skb_lab.Config.ReadConfig;
 import ru.skb_lab.files.ReadFiles;
  
 public class Main {
+	
+	private static final String PATH_CONFIG = "src/ru/skb_lab/Config/config.properties";
  
 	public static void main(String[] args) {
  
-		Properties prop = new ReadConfig().getProp();    
+		Properties prop = new ReadConfig(PATH_CONFIG).getProp();    
 		HashMap<String, String> requests = new ReadFiles(prop).getRequest();
         
     	for (String el : requests.keySet()) {
